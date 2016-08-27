1f0aee8c2207d07d6842d0b42eff3962b9d75c6f
Nitin Tulswani <tulswani19@gmail.com>
2016-08-27 21:48:36 +0530

    No need of extra array valid_emails (#1)

--- python/python-functionals/validating-email-addresses-with-a-filter.py
+++ python/python-functionals/validating-email-addresses-with-a-filter.py
@@ -1,7 +1,6 @@
 import re
 
 
-valid_emails = []
 email_regex = re.compile(r'^[a-z0-9_-]+@[a-z0-9]+\.[a-z]{1,3}$')
 emails = [raw_input() for email in range(int(raw_input()))]
 print sorted(filter(lambda email: re.match(email_regex, email), emails))
